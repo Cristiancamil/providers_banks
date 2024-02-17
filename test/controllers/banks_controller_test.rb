484a4e626e59e566db201e89dcae9efd3288b397
Cristian Lopez <crca9623@gmail.com>
2024-02-16 22:02:37 -0500

    Add changes in module bank

--- test/controllers/banks_controller_test.rb
+++ test/controllers/banks_controller_test.rb
@@ -3,23 +3,29 @@ require "test_helper"
 class BanksControllerTest < ActionDispatch::IntegrationTest
   test "Render a list of banks" do
     get banks_path
+
     assert_response :success
+    assert_select '.bank', 2
   end
 
   test "Render a single bank" do
     get bank_path(banks(:one))
+
     assert_response :success
+    assert_select '.name', 'Banco de Bogotá'
   end
 
   test "Render a new bank" do
     get new_bank_path
+
     assert_response :success
+    assert_select 'form'
   end
 
   test "Create a new bank" do
     post banks_path, params: {
       bank: {
-        name: "Test Bank", 
+        name: "Banco AV VIllas", 
         address: "CRA 16 A 25 26", 
         phone: "123456789", 
         email: "test_bank@prueba.com", 
@@ -31,7 +37,29 @@ class BanksControllerTest < ActionDispatch::IntegrationTest
         type_account: "Ahorro"
       }
     }
-    assert_response :success
+
+    assert_redirected_to banks_path
+    assert_equal flash[:notice], 'El banco se ah creado correctamente'
+
+  end
+
+  test "Dont create bank" do
+    post banks_path, params: {
+      bank: {
+        name: nil, 
+        address: nil, 
+        phone: nil, 
+        email: nil, 
+        web_site: nil, 
+        provider_nit: nil, 
+        country_code: nil, 
+        department_code: nil, 
+        city_code: nil, 
+        type_account: nil
+      }
+    }
+
+    assert_response :unprocessable_entity
   end
 
   test "Render a edit bank" do
@@ -54,7 +82,27 @@ class BanksControllerTest < ActionDispatch::IntegrationTest
         type_account: "Ahorro"
       }
     }
-    assert_response :success
+
+    assert_redirected_to banks_path
+    assert_equal flash[:notice], 'El banco se ah actualizado correctamente'
+  end
+
+  test "Dont update bank" do
+    patch bank_path(banks(:one)), params: {
+      bank: {
+        name: nil, 
+        address: nil, 
+        phone: nil, 
+        email: nil, 
+        web_site: nil, 
+        provider_nit: nil, 
+        country_code: nil,
+        department_code: nil, 
+        city_code: nil,
+        type_account: nil
+      }
+    }
+    assert_response :unprocessable_entity
   end
 
   test "Destroy a bank" do
@@ -62,7 +110,8 @@ class BanksControllerTest < ActionDispatch::IntegrationTest
       delete bank_path(banks(:one))
     end
 
-    # assert_redirected_to banks_path
-    assert_response :success
+    assert_redirected_to banks_path
+    assert_equal flash[:notice], 'El banco se ah actualizado correctamente'
   end
+  
 end
